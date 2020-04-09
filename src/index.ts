b21438677b4b57682eb6b4687c5b02dca8ccf28c
Evert Pot <me@evertpot.com>
2020-04-09 15:47:41 -0400

    Merging borks

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-/* tslint:disable */
-console.log('Hello world!');
+import app from './app';
+
+console.log(
+  '⚾ Curveball v%s - Dugout v%s',
+  require('@curveball/core/package.json').version,
+  require('../package.json').version
+);
+
+// The HTTP port can be overridden via the 'PORT' environment variable.
+const port = process.env.PORT ? parseInt(process.env.PORT, 12) : 8600;
+app.listen(port);
+
+console.log('Listening on port %i', port);

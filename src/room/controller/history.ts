8ab3570b2280a410c59389b16280894c4d41dead
Evert Pot <me@evertpot.com>
2020-04-09 22:04:34 -0400

    Multiple websockets communicating

---
+++ src/room/controller/history.ts
@@ -0,0 +1,18 @@
+import { history } from '../formats/hal';
+import { Context } from '@curveball/core';
+import * as service from '../service';
+import { resolve } from 'url';
+import Controller from '@curveball/controller';
+
+class RoomHistoryController extends Controller {
+
+  get(ctx: Context) {
+    
+    const room = service.findByPath(ctx.state.params.roomId);
+    ctx.response.body = history(room);
+
+  }
+
+}
+
+export default new RoomHistoryController();
